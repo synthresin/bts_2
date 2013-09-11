49554e6632d453a7785092de47218cfdca13e6fe
synthresin <synthresin@me.com>
2013-09-11 18:15:01 +0900

    수정

--- include/_footer.php
+++ include/_footer.php
@@ -1,5 +1,13 @@
 <div class="module span12" id="footer">
 	<div class="copyright">
-		Copyright © Big Hit Entertainment All Right Reserved.
+		<div class="row">
+			<div class="span6">
+				Copyright © Big Hit Entertainment All Right Reserved.		
+			</div>
+			<div class="span6 pull-right">
+				<a href="2kool4skool">2 KOOL 4 SKOOL PAGE</a>
+			</div>	
+		</div>
+		
 	</div>
 </div>
